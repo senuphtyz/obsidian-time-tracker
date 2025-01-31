ce341fd960fa6d86920d79f150200d543e5bd347
Bruno Lorenz <b.lorenz@afterspring.org>
2025-01-31 01:34:39 +0000

    feat: TextTaskComponent is now also used as omnisearch box

--- src/TaskTracking/UI/TextTaskComponent.tsx
+++ src/TaskTracking/UI/TextTaskComponent.tsx
@@ -1,22 +1,21 @@
-import React, { useState } from "react"
-import moment from "moment";
-import "./TextTaskComponent.scss";
 import { Play } from "lucide-react";
+import moment from "moment";
 import { TaskListEntry } from "../Types/TaskListEntry";
+import "./TextTaskComponent.scss";
 
 interface TextTaskComponentProps {
   onStart: (task: TaskListEntry) => void;
+  onSearch: (text: string) => void;
+  filter: string;
 }
 
 export const TextTaskComponent = (prop: TextTaskComponentProps) => {
-  const [inputText, setInputText] = useState<string>("");
-
   return (
     <div className="text-component">
       <div className="text">
-        <input type="text" onChange={(e) => { setInputText(e.target.value); }} />
+        <input type="text" onChange={(e) => { prop.onSearch(e.target.value) }} value={prop.filter} />
       </div>
-      <button disabled={inputText == ""} onClick={() => { prop.onStart({ last: null, path: "", start: moment(), text: inputText }) }}>
+      <button disabled={prop.filter == ""} onClick={() => { prop.onStart({ last: null, path: "", start: moment(), text: prop.filter }) }}>
         <Play
           size="18"
           color="var(--color-green)"
